c9c3b80e27846814fcf47a4465899949f081fbe7
Emilaine Figueiredo <emilaine-alves@hotmail.com>
2025-09-26 17:44:13 +0100

    inicial commit

---
+++ my-next-project/src/components/Bytes4Coolors.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+
+
+
+export default function Bytes4Coolors() {
+    return (
+        <div className="flex flex-col">
+            <div>
+                <h1 className="text-7xl text-white bg-pink-300 p-12 rounded-4xl mt-10">
+                    Paleta de Cores: Bytes4Coolors
+                </h1>
+            </div>
+            <div className="h-screen w-1/5 bg-yellow-300">
+                <h1>teste</h1>
+            </div>
+        </div>
+    );
+}
---
+++ my-next-project/src/pages/index.js
@@ -0,0 +1,9 @@
+import Bytes4Coolors from "@/components/Bytes4Coolors"
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-blue-300 flex flex-col items-center">
+      <Bytes4Coolors />
+    </div>
+  )
+}
